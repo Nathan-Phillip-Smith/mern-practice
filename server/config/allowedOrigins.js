cf79063eb65b696eeffa3fff4ed1e4156a32d5d5
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-07-24 09:46:41 -0600

    server structure created

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://www.thisisaexampleurlthatisntreal.com',
+];
+
+module.exports = allowedOrigins;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const { logEvents } = require('./middleware/logger');
 const port = process.env.PORT || 3500;
 
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
+app.use('/notes', require('./routes/noteRoutes'));
 
 app.all('*', (req, res) => {
   res.status(404);
@@ -18,6 +36,19 @@ app.all('*', (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to the DataBase');
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    mongoErrLog.log
+  );
 });
